4979924fb3d713d9b4a0e9e2015e4a6734343e04
adrian <30816241+adrianding@users.noreply.github.com>
2019-12-18 20:07:55 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
-// Import the fmt for formatting strings
-// Import os so we can read environment variables from the system
 import (
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello, Kubernetes！I'm from Jenkins CI！")
+	fmt.Println("Hello, Kubernetes！I'm from Jenkins CICD！")
 }
